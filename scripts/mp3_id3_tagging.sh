48dfa6a0b0eea47a2839e15b4b871ad2acaaf556
Chris Schammert <csmertx@gmail.com>
2024-03-03 11:48:27 -0500

    mp3_id3_tagging.sh

--- scripts/mp3_id3_tagging.sh
+++ scripts/mp3_id3_tagging.sh
@@ -3,13 +3,16 @@
 ## Works with specific filename formatting
 # 1. French 79 - Between the Buttons (Olympic).mp3
 
-MP3DIR="$1"
-cd "$MP3DIR"
-ALBUMART="$(find *.jpg)"
+### Example Usage
+## $ script "/home/user/music/albumpath"
 
-if [[ $ALBUMART == "" ]]; then
-    ALBUMART="$(find *.png)"
-fi
+## Album art types: JPG or PNG (copy to albumpath)
+## Need to convert from webp?
+# KDE Plasme Users: https://csmertx.github.io/Linux/Software/dolphin/#custom-right-click-menu
+
+MP3DIR="$1"
+
+cd "$MP3DIR"
 
 echo -en "\n\nArtist name please ==> "
 read ARTISTTAG
@@ -18,16 +21,21 @@ echo -en "\n\nAlbum title please ==> "
 read ALBUMTAG
 
 NMP3DIR="${ALBUMTAG}_tagged"
-mkdir $NMP3DIR
+mkdir "$NMP3DIR"
 
-SPCTAG=$(echo $ALBUMTAG | awk '{print substr($0,1,1)}')
+SPCTAG=$(echo "$ALBUMTAG" | awk '{print substr($0,1,1)}')
 
 echo -en "\n\nYear of album please ==> "
 read ALBUMY
 
+echo -en "\n\nAm I working with a png or jpg? ==> "
+read ALBUMAFT
+
+ALBUMART="$(find *.${ALBUMAFT} 2> /dev/null)"
+
 for f in *.mp3;
 do
-    SONGTAG="$(echo "$f" | sed "s/.*- //g" | sed "s/(${SPCTAG}.*//g")"
+    SONGTAG="$(echo "$f" | sed "s/.*- //g" | sed "s/${SPCTAG}.*//g")"
     TRACKNUM="$(echo "$f" | cut -d . -f 1)"
     id3v2 -a "$ARTISTTAG" -A "$ALBUMTAG" -t "$SONGTAG" -y "$ALBUMY" -T "$TRACKNUM" "$f"
     ffmpeg -i "$f" -i "$ALBUMART" -map_metadata 0 -map 0 -map 1 "${NMP3DIR}/${f}"
